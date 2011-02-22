cce33c7561cd198b13c32219f08f63d212ce0433
Johan Sveholm <jsveholm@gmail.com>
2011-02-22 10:15:34 +0900

    special section can now be added to form with javascript

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,7 +27,6 @@ class PagesController < ApplicationController
 
   def edit
     @page = Page.find(params[:id])
-    @page.sections.build
   end
 
   def update
---
+++ app/models/list_section.rb
@@ -0,0 +1,2 @@
+class ListSection < Section
+end
