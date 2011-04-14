56636fede02589f34889931ee83772bca7e1884f
Johan Sveholm <jsveholm@gmail.com>
2011-04-14 14:41:37 +0900

    routing hack

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,8 @@ namespace :deploy do
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/uploads_private #{release_path}/private/uploads"
-    run "ln -nfs #{shared_path}/uploads_public #{release_path}/public/uploads"    
+    run "ln -nfs #{shared_path}/uploads_public #{release_path}/public/uploads"
+    run "ln -nfs #{shared_path}/config/.htaccess #{release_path}/public/.htaccess"
   end
 end
 
